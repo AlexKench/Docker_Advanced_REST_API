9ef2bda2c056345b683393eb3422724c3aa4012f
Alexander <sasha.maks92@gmail.com>
2023-04-15 19:34:36 +0300

    +

--- src/main/java/ru/kata/spring/boot_security/demo/models/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -25,6 +25,12 @@ public class User {
     @Column(name = "sex")
     private String sex;
 
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
     public User() {
 
     }
@@ -74,6 +80,22 @@ public class User {
 
     public void setSex(String sex) {
         this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
