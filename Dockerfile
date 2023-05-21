98345424ce2afcc7affb02639ba95e94e9bcf878
Alexander <sasha.maks92@gmail.com>
2023-05-21 22:01:25 +0300

    +

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM openjdk
+ADD /target/spring-boot_security-demo-0.0.1-SNAPSHOT.jar backend.jar
+ENTRYPOINT ["java", "-jar", "backend.jar"]
--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -7,8 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootSecurityDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
-	}
 
-}
+
+	public static void main(String[] args) throws InterruptedException {
+		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+		Thread.sleep(1000);
+	}}
