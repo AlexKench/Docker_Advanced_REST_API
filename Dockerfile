4e7f983e7627f660a6485b0a4c6c2726d27d0e0b
Alexander <sasha.maks92@gmail.com>
2023-05-27 18:52:17 +0300

    Docker and mysql

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM openjdk
-ADD /target/spring-boot_security-demo-0.0.1-SNAPSHOT.jar backend.jar
+
+COPY target/spring-boot_security-demo-0.0.1-SNAPSHOT.jar backend.jar
+
 ENTRYPOINT ["java", "-jar", "backend.jar"]
---
+++ src/test/java/RestApiControllerTest.java
@@ -0,0 +1,24 @@
+//import org.junit.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//import ru.kata.spring.boot_security.demo.controllers.RestApiController;
+//import ru.kata.spring.boot_security.demo.util.UserNotCreatedException;
+//@Component
+//public class RestApiControllerTest {
+//
+//
+//    private final RestApiController restApiController;
+//
+//    @Autowired
+//    public RestApiControllerTest(RestApiController restApiController) {
+//        this.restApiController = restApiController;
+//    }
+//
+//    @Test(expected = UserNotCreatedException.class)
+//    public void testEx() {
+//
+//        restApiController.createNewUser()
+//
+//
+//    }
+//}
